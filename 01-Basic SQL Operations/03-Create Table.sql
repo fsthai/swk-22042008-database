f8d4921b38b94aa28a2b8c1e865aa14c6aedc898
Chrisn Th <51808725+fsthai@users.noreply.github.com>
2020-09-16 09:22:48 +0700

    Create 03-Create Table.sql

---
+++ 01-Basic SQL Operations/03-Create Table.sql
@@ -0,0 +1,30 @@
+# Sriworakarn College by Aj Krit Th.
+# Copyrighted krit.th@swkmail.com
+
+
+# SQL สร้างตาราง
+/*
+CREATE TABLE <ชื่อตาราง>
+(
+  <ชื่อหัวโครงข้อมูล 1> <ประเภทข้อมูล>,
+  <ชื่อหัวโครงข้อมูล 2> <ประเภทข้อมูล>,
+  ...,
+  <ชื่อหัวโครงข้อมูล 3> <ประเภทข้อมูล>
+)
+*/
+
+CREATE TABLE cats
+(
+  name VARCHAR(15),
+  bleed VARCHAR(140),
+  age INT
+)
+
+
+# SQL ลบตาราง
+# DROP TABLE <ชื่อตารางที่ต้องการลบ>
+DROP TABLE cats
+
+
+# Sriworakarn College by Aj Krit Th.
+# Copyrighted krit.th@swkmail.com
