437943329a7c7118de603bdfadba81a3886329a3
Chrisn Th <51808725+fsthai@users.noreply.github.com>
2020-09-16 09:18:01 +0700

    Create 02-Creating a Database.sql

---
+++ 01-Basic SQL Operations/02-Creating a Database.sql
@@ -0,0 +1,26 @@
+# Sriworakarn College by Aj Krit Th.
+# Copyrighted krit.th@swkmail.com
+
+# SQL แสดงรายชื่อฐานข้อมูลทั้งหมดในระบบฐานข้อมูลที่เรามี
+show databases;
+
+
+# SQL สร้างฐานข้อมูลใหม่
+# CREATE DATABASE <ชื่อฐานข้อมูลทีต้องการสร้าง>
+CREATE DATABASE dog;
+CREATE DATABASE school_store;
+CREATE DATABASE users;
+
+
+# SQL ลบฐานข้อมูลออกจากระบบ
+# DROP DATABASE <name>;
+DROP DATABASE dog;
+DROP DATABASE school_store;
+
+# SQL เรียกใช้งานฐานข้อมูล ..ต้องเรียกทุกครั้งก่อนดำเนินการอะไรก็ตามกับฐานข้อมูลนั้น
+# USE <database name>;
+USE dog;
+
+
+# Sriworakarn College by Aj Krit Th.
+# Copyrighted krit.th@swkmail.com
