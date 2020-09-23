c31aa19d1f873217868cdce4718dcddc4d1e2cfa
Chrisn Th <51808725+fsthai@users.noreply.github.com>
2020-09-23 08:10:57 +0700

    Create 04-ลบข้อมูลจากตาราง DELETE.sql

---
+++ 02-CRUD/04-ลบข้อมูลจากตาราง DELETE.sql
@@ -0,0 +1,12 @@
+-- Sriworakarn College by Aj.Krit Th.
+-- Copyrighted krit.th@swkmail.com
+
+
+# SQL ลบข้อมูลในตารางข้อมูล
+/*
+DELETE FROM ชื่อตาราง WHERE เงื่อนไข;
+*/
+
+-- ลบข้อมูลจากตาราง cats แถวข้อมูลที่ id = 11
+DELETE FROM cats WHERE id = 11
+;
